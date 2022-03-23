1e55b7188ad28396795e5d034806cb6f19446124
PatilDarsh <darshpatil072@gmail.com>
2022-03-23 09:29:29 +0530

    ADDED:[Darshan] progrem for sub of 2 Numbers

---
+++ SUB.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter the 2 No:");
+	scanf("%d %d",&a,&b);
+	c=a-b;
+	printf("\n Sub is:%d",c);
+	getch();
+}
